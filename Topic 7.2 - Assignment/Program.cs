2b12fd3d331e8ff6f8b90aafabc2a34399fa3e02
Emmett Cornies <339761330@GoTVDSB.ca>
2024-10-18 09:38:58 -0400

    Add project files.

---
+++ Topic 7.2 - Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Schema;
+
+namespace Topic_7._2___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LOOK! THE ROCKET'S BLASTING OFF!!!");
+            Console.WriteLine();
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine();
+            Console.WriteLine("BLAST OFF!!");
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Do you like tables of value? I do too! Look at this one I worked very hard on!");
+            Console.WriteLine();
+            Console.WriteLine("\t X \t Y");
+            Console.WriteLine("\t------------");
+            for (double i = -10; i >= 10; i += 2)
+            {
+                double xVar = i, yVar = Math.Pow(i, 2);
+                Console.WriteLine("\t" + xVar + "|\t" + yVar);
+            }
+        }
+    }
+}
